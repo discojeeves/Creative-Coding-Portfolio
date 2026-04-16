4b05bbe99c583109407dcda48c36f0a3fb550cd4
liambutler <liambutler319@gmail.com>
2026-04-16 10:02:30 -0400

    add sdPyramid, general cleanup

--- projects/frag.glsl
+++ projects/frag.glsl
@@ -41,12 +41,27 @@ float sdTorus( vec3 pos, vec2 t ) {
     vec2 q = vec2(length(pos.xz)-t.x,pos.y);
     return length(q)-t.y;
 }
+float sdPyramid( vec3 p, float height) {
+  float m2 = height*height + 0.25;
+
+  p.xz = abs(p.xz);
+  p.xz = (p.z>p.x) ? p.zx : p.xz;
+  p.xz -= 0.5;
+
+  vec3 q = vec3( p.z, height*p.y - 0.5*p.x, height*p.x + 0.5*p.y);
+  float s = max(-q.x,0.0);
+  float t = clamp( (q.y-0.5*p.z)/(m2+0.25), 0.0, 1.0 );
+  float a = m2*(q.x+s)*(q.x+s) + q.y*q.y;
+  float b = m2*(q.x+0.5*t)*(q.x+0.5*t) + (q.y-m2*t)*(q.y-m2*t);
+
+  float d2 = min(q.y,-q.x*m2-q.y*0.5) > 0.0 ? 0.0 : min(a,b);
+  return sqrt( (d2+q.z*q.z)/m2 ) * sign(max(q.z,-p.y));
+}
 
 float sdLink( vec3 pos, float le, float r1, float r2 ) {
     vec3 q = vec3( pos.x, max(abs(pos.y)-le,0.0), pos.z );
     return length(vec2(length(q.xy)-r1,q.z)) - r2;
 }
-
 
 
 //------  Joining Operators  ------\\
@@ -89,10 +104,10 @@ vec2 bsUnionMat(float obj1, float matID1, float obj2, float matID2) {
     else             return vec2(obj2, matID2);
 }
 
-vec2 smUnionMat(float obj1, float mat1, float obj2, float mat2, float k) {
+vec2 smUnionMat(float obj1, float matID1, float obj2, float matID2, float k) {
     float h = clamp ( 0.5 + 0.5*(obj2-obj1)/k, 0.0, 1.0);
     float result = mix(obj2, obj1, h) - k*h*(1.0-h);
-    float matblend = mix(mat2, mat1, h);
+    float matblend = mix(matID2, matID1, h);
     return vec2(result , matblend);
 }
 
@@ -113,43 +128,32 @@ mat2 degrot2D(float angle) {
     return mat2(c, -s, s, c); 
 }
 
-
 // ***** ***** ***** Scene Time ***** ***** *****\\
 
 
 vec2 map(vec3 pos) {
-    float box = sdBox(pos, vec3(0.75));
-    vec3 spherePos = vec3( sin(u_time), cos(u_time), 0.0);
-    float sphere = sdSphere(pos - spherePos, 0.75);
+    float box = sdBox(pos, vec3(0.6)) -0.25;
+    vec3 spherePos = vec3(0.9 * cos(u_time), sin(u_time)*0.0625, 0.0);
+    float sphere = sdSphere(pos - spherePos, 0.5);
 
-    return smUnionMat(box, 0.0, sphere, 2.0, 0.1);
+    float pyramid = sdPyramid(vec3(pos.x, pos.y + 0.5, pos.z), 1.0);
 
-
-
+    return smUnionMat(pyramid, 1.0, sphere, 2.0, 0.5);
 }
 
 vec3 getProceduralColor(float id) {
-    return 0.5 + 0.5 * cos(id * 1.0 + vec3(0.0, 0.0, 0.0));
+    return 0.5 + 0.5 * cos(id * 1.0 + vec3(0.0, 2.0, 4.0));
 }
 
 
-
-vec3 getSpecifiedColor(float mat) {
-    if (mat == 1.0) return vec3(1.0, 0.0, 0.0); // red 
-    if (mat == 2.0) return vec3(0.0, 0.0, 1.0); // blue 
-    
-    return vec3(0.0); // fallback to black
-
+vec3 calc_normal(vec3 pos) {
+    float e = 0.0001;
+    return normalize(vec3(
+        map(pos + vec3(e, 0, 0)).x - map(pos - vec3(e, 0, 0)).x,
+        map(pos + vec3(0, e, 0)).x - map(pos - vec3(0, e, 0)).x,
+        map(pos + vec3(0, 0, e)).x - map(pos - vec3(0, 0, e)).x
+    ));
 }
-
-// vec3 calc_normal(vec3 pos) {
-//     float e = 0.0001;
-//     return normalize(vec3(
-//         map(pos + vec3(e, 0, 0)) - map(pos - vec3(e, 0, 0)),
-//         map(pos + vec3(0, e, 0)) - map(pos - vec3(0, e, 0)),
-//         map(pos + vec3(0, 0, e)) - map(pos - vec3(0, 0, e))
-//     ));
-// }
 
 
 
@@ -174,12 +178,12 @@ vec2 rayMarch(vec3 ray_origin, vec3 ray_dir) {
 
 void main() {
     vec2 uv = vUv.xy;       
-    vec3 ray_origin = u_camPos;                                              // ray origin
-    vec3 ray_dir = (u_camInvProjMat * vec4(uv*2. -1., 0, 1)).xyz;            // ray direction
+    vec3 ray_origin = u_camPos;                                             
+    vec3 ray_dir = (u_camInvProjMat * vec4(uv*2. -1., 0, 1)).xyz;          
     ray_dir = (u_camToWorldMat * vec4(ray_dir,0)).xyz;
     ray_dir = normalize(ray_dir);
     
-    vec2 result = rayMarch(ray_origin, ray_dir); // t is total distance travelled
+    vec2 result = rayMarch(ray_origin, ray_dir); 
     float total_dist = result.x;
     float mat = result.y;
 
